bed2a4a7d987707a9a9dfb1b46d11c2c3a1ebce8
Md Robin Rana <developer.smrobin45@gmail.com>
2023-10-17 12:45:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+   res.send("server is running");
+});
+
+app.listen(port, () => {
+   console.log("Server listening");
+});
